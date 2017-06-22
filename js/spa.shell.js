a0c1056ec8ead6766652f665d7d371022a107f64
Leesuyun <qwuse87@gmail.com>
2017-06-22 17:46:58 +0900

    채팅 슬라이더를 여닫는 기능을 추가한 셸

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -27,12 +27,16 @@ spa.shell = (function (){
 			+ '</div>'
 			+ '<div class="spa-shell-foot"></div>'
 			+ '<div class="spa-shell-chat"></div>'
-			+ '<div class="spa-shell-modal"></div>'
+			+ '<div class="spa-shell-modal"></div>',
+			chat_extend_time : 1000,
+			chat_retract_time : 300,
+			chat_extend_height : 450,
+			chat_retract_height : 15
 		},
 		stateMap = { $container : null },
 		jqueryMap = {},
 
-		setJqueryMap, initModule;
+		setJqueryMap, toggleChat, initModule;
 	// ---------------- 모듈 스코프 변수 끝 ----------------
 
 	// ---------------- 유틸리티 메서드 시작 ----------------
@@ -42,9 +46,61 @@ spa.shell = (function (){
 	// DOM 메서드 /setJqueryMap/ 시작
 	setJqueryMap = function(){
 		var $container = stateMap.$container;
-		jqueryMap = { $container : $container };
+		jqueryMap = { 
+			$container : $container,
+			$chat : $container.find( '.spa-shell-chat' )
+		};
 	}
 	// DOM 메서드 /setJqueryMap/ 끝
+
+	// DOM 메서드 /toggleChat/ 시작
+	// 목적 : 채팅 슬라이더 영역을 열고 닫는다.
+	// 인자 :
+	// * do_extend - true면 열고, false면 닫는다.
+	// * callback - 애니메이션 종류 시점에 callback 함수를 실행한다.
+	// 설정 :
+	// * chat_extend_time, chat_retract_time
+	// * chat_extend_height, chat_retract_height
+	// 반환값 : bolean
+	// * true - 슬라이더 애니메이션이 실행된다.
+	// * false - 슬라이더 애니메이션이 실행되지 않는다.
+	// 
+	toggleChat = function ( do_extend, callback ){
+		var
+			px_chat_ht 	= jqueryMap.$chat.height(),
+			is_open 	= px_chat_ht === configMap.chat_extend_height,
+			is_closed	= px_chat_ht === configMap.chat_retract_height,
+			is_sliding	= ! is_open && ! is_closed;
+
+			if( is_sliding ) { return false; }
+
+			// 채팅 슬라이더 확장 시작
+			if( do_extend )	{
+				jqueryMap.$chat.animate(
+					{ height : configMap.chat_extend_height},
+					configMap.chat_extend_time,
+					function(){
+						if( callback ){ callback(jqueryMap.$chat); }
+					}
+				);
+				return true;
+			};
+			// 채팅 슬라이더 확장 끝
+
+			// 채팅 슬라이더 축소 시작
+			jqueryMap.$chat.animate(
+				{ height : configMap.chat_retract_height },
+				configMap.chat_retract_time,
+				function( ){
+					// 경쟁 조건을 피한다.
+					if( callback ){ callback( jqueryMap.$chat); }
+				}
+			);
+			return true;
+	}
+	// 채팅 슬라이더 축소 끝
+	// DOM 메서드 /toggleChat/ 끝
+
 	// ---------------- DOM 메서드 끝 ----------------
 
 	// ---------------- 이벤트 핸들러 시작 ----------------
@@ -53,9 +109,14 @@ spa.shell = (function (){
 	// ---------------- Public 메서드 시작 ----------------
 	// public 메서드 /initModule/ 시작
 	initModule = function ( $container ){
+		// HTML을 로드한 후 제이쿼리 컬랙션 객체를 매핑한다.
 		stateMap.$container = $container;
 		$container.html( configMap.main_html );
 		setJqueryMap();
+
+		// 토글 테스트
+		setTimeout( function() {toggleChat( true );}, 3000);
+		setTimeout( function() {toggleChat( false );}, 8000);
 	}
 	// public 메서드 /initModule/ 끝
 
